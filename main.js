d2b3a2becb398ecfda8d1838aa8059a00fbef7ff
LOKESH KUMAR <easterscience@gmail.com>
2021-03-04 12:21:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ let curr_track = document.createElement('audio');
 // Define the tracks that have to be played
 let track_list = [
   {
-    name: "Night Owl",
-    artist: "Broke For Free",
+    name: "NEFFEX Cold",
+    artist: "NEFFEX",
     image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "https://easterscience.github.io/easterplayer/songs/neffex/Cold.mp3"
   },
