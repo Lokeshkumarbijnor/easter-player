3adc9a7543d36b22b6421af9207cbfa3223a0dc5
LOKESH KUMAR <easterscience@gmail.com>
2021-03-07 20:12:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ let curr_track = document.createElement('audio');
 let track_list = [
   
   {
-    name: "It's So Cold",
+    name: "It's So Cold+",
     artist: "NEFFEX",
     image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "https://easterscience.github.io/easterplayer/songs/neffex/Cold.mp3"
